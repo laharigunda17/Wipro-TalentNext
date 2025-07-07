66fd6c1552549227f3789031341486205689e4c7
laharigunda17 <laharigunda17@gmail.com>
2025-07-07 10:30:57 +0530

    July 7 - java fundamentals

---
+++ java_fundamentals/src/jfub/Question2.java
@@ -0,0 +1,14 @@
+package jfub;
+
+import java.util.Scanner;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		System.out.println("Welcome "+s1);
+
+	}
+
+}
---
+++ java_fundamentals/src/jfub/Question4.java
@@ -0,0 +1,10 @@
+package jfub;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
